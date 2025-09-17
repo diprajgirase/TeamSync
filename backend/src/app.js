3df1b7e72b012f51904996665a6aafdf1bd4b5ef
Dipraj Girase <girasedipraj52@gmail.com>
2025-09-17 19:23:51 +0530

    feat(frontend): setup completed with UI implementation

--- backend/src/app.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv')
-dotenv.config();
-const express = require("express")
-const cors = require("cors")
-const app = express()
-
-app.use(cors());
-
-module.exports = app
